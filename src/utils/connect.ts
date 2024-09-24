9a1814d99267a79c931baa37a2b97b4f05c2949f
Arsalaan Khan <arslaan.tech@gmail.com>
2024-09-24 12:17:27 +0530

    first push

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
 import config from "config";
-import logger from "./logger";
 
 async function connect() {
-  const dbUri = config.get<string>("dbUri");
-
-  try {
-    await mongoose.connect(dbUri);
-    logger.info("DB connected");
-  } catch (error) {
-    logger.error("Could not connect to db");
-    process.exit(1);
-  }
+    const dbUri = config.get<string>("dbUri");
+    try {
+        await mongoose.connect(dbUri)
+        console.log("db connected")
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
 }
 
-export default connect;
+export default connect
